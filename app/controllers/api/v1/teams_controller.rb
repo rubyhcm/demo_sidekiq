63ff34053840b32dbf30db3cf3b7e8a7883d5eff
nguyenloi <loindp@ipinc.io>
2024-01-04 22:39:10 +0700

    Modify teams_controller

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,14 +1,17 @@
 class Api::V1::TeamsController < ApplicationController
   def index
-    teams = Team.all.select(:id, :name, :rating)
-    render json: {data: teams}
+    teams = Team.select(:id, :name, :rating)
+    render json: { data: teams }
   end
+
   def show
     team = Team.where(id: team_params[:id]).select(:id, :name, :rating)
     BuyTimeWorker.perform_async(team.first.name, team.first.rating)
-    render json: {data: team}
+    render json: { data: team }
   end
+
   private
+
   def team_params
     params.permit(:id)
   end
